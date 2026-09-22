93de6ea544333c1b5ab9d57e87a50decc4702779
Ahmed Said <ahmeds3id711@gmail.com>
2026-09-22 14:00:21 +0300

    Add UploadImageRequestValidator for image validation

---
+++ FileManagerApi/Contracts/UploadImageRequestValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+
+namespace FileManagerApi.Contracts
+{
+    public class UploadImageRequestValidator:AbstractValidator<UploadImageRequest>
+    {
+        private readonly string[] _allowedExtensions = [".jpg", ".jpeg", ".png", ".gif", ".bmp"];
+        public UploadImageRequestValidator()
+        {
+            RuleFor(x=>x.Image)
+                .SetValidator(new FileSizeValidator())
+                .SetValidator(new LockedSignatureValidator());
+
+            RuleFor(x=>x.Image)
+                .Must(x =>
+                {
+                    var extensions = Path.GetExtension(x.FileName.ToLower());
+                    return _allowedExtensions.Contains(extensions);
+                })
+                .WithMessage("File must be an image")
+                .When(x => x.Image != null);
+        }
+    }
+}
